1aad4d1a0d090b2b3dce580b7fc5a8d81ee53de3
code-woncheol <codewc23@gmail.com>
2024-05-01 16:32:35 +0900

    final

---
+++ midterm/src/components/Toggle.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Toggle() {
+    const [isToggleOn, setIsToggleOn] = useState(true);
+
+    const handleClick = () => {
+        setIsToggleOn(prevState => !prevState);
+    };
+
+    return (
+        <button onClick={handleClick}>
+            {isToggleOn ? 'ON' : 'OFF'}
+        </button>
+    );
+}
